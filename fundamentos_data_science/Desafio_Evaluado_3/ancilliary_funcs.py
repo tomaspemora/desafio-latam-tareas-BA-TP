f5ea5614135ad11971250a44f39952964656d184
Braulio <baguila.a@gmail.com>
2022-07-03 23:31:29 -0400

    Actualización con subsample BA

--- fundamentos_data_science/Desafio_Evaluado_3/ancilliary_funcs.py
+++ fundamentos_data_science/Desafio_Evaluado_3/ancilliary_funcs.py
@@ -1,6 +1,28 @@
-def puntaje_z(x):
-    return (x - x.mean()) / x.std()
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def null_analyzer(dataframe, var, print_list =False):
+    totales = dataframe[var].isnull().sum()
+    print(var)
+    print(f"La cantidad de registros perdidos es {totales}, {totales/len(dataframe[var])}")
+    print(f"*************************\n")
 
 
-def puntaje_z_norm(x):
-    return (x - x.mean()) / x.std()
+def puntaje_z(dataframe, col):
+    media = dataframe[col].mean()
+    desv_est = dataframe[col].std()
+
+    ptje_z = (dataframe[col] - media) / desv_est
+    return ptje_z
+
+
+
+
+#def puntaje_z(x):
+    #return (x - x.mean()) / x.std()
+
+
+#def puntaje_z_norm(x):
+    #return (x - x.mean()) / x.std()
