34b8c46a6c5fd129eb5c07c81a3216f6c8557c1e
tomaspemora <tomaspemora@gmail.com>
2022-07-31 22:44:54 -0400

    subida desafio_1

---
+++ fundamentos_data_science/Prueba/helpers.py
@@ -0,0 +1,49 @@
+import numpy as np
+from IPython.display import display
+import pandas as pd
+
+def pretty(d, indent=0):
+    for key, value in d.items():
+        print('\t' * indent + str(key), end='')
+        if isinstance(value, dict):
+            print()
+            pretty(value, indent+1)
+            
+        else:
+            print(':' * (1) + str(value))
+
+def encoding_feature(x): return dict(zip(range(len(x)),x))
+
+def make_pretty(styler, num_format='{:.2f}'):
+    d1 = dict(selector="td",props=[('text-align', 'center')])
+    d2 = dict(selector="th",props=[('text-align', 'center')])
+    d3 = dict(selector=".index_name",props=[('text-align', 'center')])
+    d4 = dict(selector="th.col_heading",props=[('text-align', 'center')])
+    styler.format(num_format)
+    styler.format_index(lambda v: v.upper())
+    styler.background_gradient(axis=None, cmap="YlGnBu")
+    styler.set_table_styles([d1,d2,d3,d4])
+    styler.set_properties(**{'border': '1px black solid !important', 'text-align': 'center'})
+    styler.set_table_styles([{'selector': 'th','props': [('border', '2px black solid !important'), ('min-width','90px'), ('max-width','90px'), ('width','90px'), ('text-align','center')]}])
+    styler.set_table_styles([{'selector': 'td','props': [('border', '2px black solid !important'), ('min-width','90px'), ('max-width','90px'), ('width','90px'), ('text-align','center')]}])
+    styler.set_table_styles([{'selector': '.index_name','props': [('border', '2px black solid !important'), ('min-width','90px'), ('max-width','90px'), ('width','90px'), ('text-align','center')]}])
+    styler.set_table_styles([{'selector': 'th.col_heading','props': [('border', '2px black solid !important'), ('min-width','90px'), ('max-width','90px'), ('width','90px'), ('text-align','center')]}])
+    styler.applymap_index(lambda v: "min-width:90px;max-width:90px;width:90px", axis=0)
+    return styler
+
+def get_type_vars(df):
+    cat_variables = df.select_dtypes(include=['object','string']).columns # Variables categóricas
+    num_variables = df.select_dtypes(include=['number']).columns # Variables numéricas
+    return (cat_variables, num_variables)
+
+def describe_variables(df):
+    cat_variables,_ = get_type_vars(df)    
+    print("------------------------------------------------------------")
+    print("-------------------Variables Categóricas--------------------")
+    print("------------------------------------------------------------")
+    for col in cat_variables:
+        display(pd.DataFrame(df[col].value_counts()).T.style.pipe(make_pretty, num_format='{:d}'))
+    print("------------------------------------------------------------")
+    print("-------------------Variables Numéricas----------------------")
+    print("------------------------------------------------------------")
+    display(df.select_dtypes(include=['number']).describe().T.style.pipe(make_pretty, num_format='{:.1f}'))
